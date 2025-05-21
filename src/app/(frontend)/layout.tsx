957ec4c9c651674672306209b0e773c49c4a0c87
DiegoSalazar <d.salazararp@gmail.com>
2025-05-21 01:53:14 -0400

    setting development enviroment

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,19 @@
+import { SanityLive } from "@/sanity/lib/live";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+        <SanityLive />
+
+      </body>
+    </html>
+  );
+}
