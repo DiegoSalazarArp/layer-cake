cefecaebe687eb77df16cf677e5a9f220bb18eac
DiegoSalazar <d.salazararp@gmail.com>
2025-05-21 21:07:13 -0400

    add related post component from the studio

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -56,6 +56,11 @@ export const postType = defineType({
       name: 'body',
       type: 'blockContent',
     }),
+    defineField({
+      name: 'relatedPosts',
+      type: 'array',
+      of: [defineArrayMember({ type: 'reference', to: { type: 'post' } })],
+    }),
   ],
   preview: {
     select: {
